b3d927279fd9514e3bd3eb72998d725bc9150063
ketoni <juhanakekoni@yahoo.com>
2026-01-31 10:19:26 +0200

    Extend MaskStats

--- Assets/MaskStats.cs
+++ Assets/MaskStats.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Mask", menuName = "Mask Stats")]
@@ -6,5 +8,49 @@ public class MaskStats : ScriptableObject
 {
     public bool cursed;
 
+    public List<MaskRule> rules;
+
     // Add more...
+}
+
+[Serializable]
+public struct MaskRule
+{
+    public MaskFeature first;
+    public bool requireSecond;
+    public MaskFeature second;
+    public MaskEffect effect;
+}
+
+public enum MaskFeature
+{
+    None,
+    Horns,
+    Eyes,
+    Ears,
+    Nose,
+    Mouth,
+}
+
+public enum MaskEffect
+{
+    AddTime,
+    AddSanity,
+    LowCurse,
+    MedCurse,
+    HighCurse,
+    Cheap,
+    Cheaper,
+    Valuable,
+    Valuabler,
+    DoublePrice,
+    Key,
+    NegateKey,
+    NegateBless,
+    RepeatCurse,
+    CollectedCountValue,
+    ClockStop,
+    NoCleanse,
+    SlowCleanse,
+    Possessed,
 }
