85eb175337cec3988af43c453a6fbb3388b9715c
ketoni <juhanakekoni@yahoo.com>
2026-02-01 04:23:58 +0200

    fix build :D

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
